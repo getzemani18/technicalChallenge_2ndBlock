8157940e805d042a86083c1ed4bfd3e251898259
getzemani18 <amandaalvarado0129@gmail.com>
2022-01-12 17:02:36 -0600

    carrousel dinamico y subir babel

--- src/firebase.js
+++ src/firebase.js
@@ -17,20 +17,23 @@
         const app = initializeApp(firebaseConfig);
 
         import {
-            getFirestore, collection, addDoc
+            getFirestore, collection, addDoc, doc, getDocs
         }
         from "https://www.gstatic.com/firebasejs/9.6.2/firebase-firestore.js";
 
         const db = getFirestore()
 
-        // Referencias
-        // let title = document.getElementById("task-title")
-        // let description = document.getElementById("task-description")
-        // let type = document.getElementById("type")
-        // let month = document.getElementById("mes")
-        // let dia = document.getElementById("dia")
+        let today = new Date()
+        let todayString = today.toDateString()
+    
+        console.log(today);
+        console.log(todayString);
 
+        // Referencias
+    
         let saveBtn = document.getElementById("guardar")
+
+        
 
         // Agregar documentos
         const  addDocument = async () => {
@@ -39,58 +42,82 @@
                 Title:  document.getElementById("task-title").value,
                 DEscr: document.getElementById("task-description").value,
                 Tipo: document.getElementById("type").value,
-                Mes: document.getElementById("mes").value
+                Mes: document.getElementById("mes").value,
+                date: today,
+                dateS : todayString
             });
             console.log("Document written with ID: ", docRef.id);
             } catch (e) {
             console.error("Error adding document: ", e);
-            }
+            }   
         };
 
+       
         // Asigando eventos
         saveBtn.addEventListener("click", addDocument)
 
 
-        // dINAMICO
         let carrousel = document.getElementById("carrousel")
+        const getAllData = async () => {
+            const querySnapshot = await getDocs(collection(db, "users"))
+
+            var notas = []
+
+            querySnapshot.forEach(doc => {
+                notas.push(doc.data())
+                console.log(doc.data())
+                console.log(`${doc.id}`);
 
 
-        db.collection("users").onSnapshot((querySnapshot) => {
-            carrousel.innerHTML =""
-            querySnapshot.forEach((doc) => {
-                carrousel.innerHTML += `
-                <div class="card">
+                carrousel.innerHTML += 
+                `<div class="card">
                 <div class="card-header">
                     <h4>Notes</h4>
                 </div>
                 <div class="card-body">
-                    <div id="miCarousel" class="carousel slide" data-bs-ride="carousel">
+                    <div id="${doc.id}" class="carousel slide" data-bs-ride="carousel">
                         <div class="carousel-indicators">
-                            <button type="button" data-bs-target="#miCarousel" data-bs-slide-to="0"
+                            <button type="button" data-bs-target="#${doc.id}" data-bs-slide-to="0"
                                 class="active"></button>
-                            <button type="button" data-bs-target="#miCarousel" data-bs-slide-to="0"
+                            <button type="button" data-bs-target="#${doc.id}" data-bs-slide-to="0"
                                 class="active"></button>
                         </div>
                         <div class="carousel-inner">
                             <div class="carousel-item active">
-                                <h3>${doc.data().Title}</h3>
+                                <h1>${doc.data().Title}</h1>
+                                <h2>${doc.data().DEscr}</h2>
+                                <h2>${doc.data().Tipo}</h2>
+                                <h2>${doc.data().Mes}</h2>
+                                <h1>${doc.data().date.toDate()}</h1>
                             </div>
-                            <div class="carousel-item">
-                                <h5>jkec</h5>
-                            </div>
+                        
                         </div>
-                        <button class="carousel-control-prev" type="button" data-bs-target="#miCarousel"
+                        <button class="carousel-control-prev" type="button" data-bs-target="#${doc.id}"
                             data-bs-slide="prev">
                             <span class="carousel-control-prev-icon" aria-hidden="true"></span>
 
                         </button>
-                        <button class="carousel-control-next" type="button" data-bs-target="#miCarousel"
+                        <button class="carousel-control-next" type="button" data-bs-target="#${doc.id}"
                             data-bs-slide="next">
                             <span class="carousel-control-next-icon" aria-hidden="true"></span>
 
                         </button>
                     </div>
                 </div>
-            </div>`
+            </div>
+                `
+
             })
-        })
+        }
+       getAllData()
+        // .forEach((doc) => {
+           
+        
+
+
+        // dINAMICO
+       
+        
+
+
+      
--- src/notesAdd.js
+++ src/notesAdd.js
@@ -3,48 +3,29 @@ console.log(agregar)
 
 let arrayMes = [];
 
+
 const traerMes = () => {
     fetch(agregar)
     .then((response) => response.json())
     .then((data) => {
         arrayMes.push(data)
-        
-        // Iterar Meses
-        // for (const months in arrayMes[0]["Meses"]) {
-        //    console.log(months);
-        //    let mesesImp = document.getElementById("mes")
-        //    mesesImp.innerHTML += `<option></option>`
-        // }
-
-        // Iterar DIas
-        for (const dias of arrayMes[0].Meses) {
-            // console.log(dias);
-            let mesesImp = document.getElementById("mes")
-             mesesImp.innerHTML += `<option>${dias.Mes}</option>`
-             
-        }
-
+        iterarMeses(arrayMes[0].Meses)
+       
+    
 })
 }
 
-traerMes()
+
 // Iterando
-// const iterarMes = (arraydeMes) => {
-//     for( const [index, mes] of arraydeMes.entries()){
-//         let meses = document.getElementById("mes")
-//         meses.innerHTML += `<option onclick = "slectmes('${index}')">${mes.Mes}</option>`
-//         // console.log(meses)
-//     }
-// }
+const iterarMeses = (arrayMes) => {
+    for( const [index, mes] of arrayMes.entries()){
+        let meses = document.getElementById("mes")
+        meses.innerHTML += `<option>${mes.Mes}</option>`
+        // console.log(meses)
+    }
+}
 
-// const iterarDia = (index) => {
-//     for (const dia of Meses[0].Dia) {
-//         console.log(Meses[0].Dia);
-//         let dias = document.getElementById("dia")
-//         dias.innerHTML += `<option>${dia}</option>`
-//         console.log(dias);
-//     }
-// }
 
+traerMes()
 
 
